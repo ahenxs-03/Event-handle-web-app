591ce58c1cb1f62285ccfeea528534f4d101429f
ahenxs-03 <bhattacharjeetapati0@gmail.com>
2026-01-29 17:50:41 +0000

    session added

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ class Config:
     Test=False
     SQLALCHEMY_DATABASE_URI = "sqlite:///mydatabase.db"
     SECRET_KEY = "super_secret"
+    SESSION_REFRESH_EACH_REQUEST = False
+    SESSION_COOKIE_SECURE = True
+    SESSION_PERMANENT = True
 
 
--- home.py
+++ home.py
@@ -2,36 +2,39 @@ from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 from flask import session
 from flask import Blueprint
-from flask_login import LoginManager, logout_user, login_user
+from flask_login import LoginManager, logout_user, login_user, login_required
 
 #from app import create
 from flask import render_template, url_for, request, redirect, flash
 from db import Role, User, Host, Student
-sessions={
-    "user_role":''
 
-}
 #app=create()
 # login maneger create
-#manager = LoginManager(app)
+manager = LoginManager()
 log=Blueprint('log','__name__')
-
-#@manager.user_loader
+manager.view='login'
+@manager.user_loader
 def load_user(user_id):
-    User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 @log.route('/', methods=['GET', 'POST'])
 def login():
+    
     if request.method == "GET":
+        print('get response')
+        print(session)
         return render_template('home.html')
     if request.method == 'POST':
+        print("post")
+        print(session)
         
         #flash('Wrong')
         name = request.form["name"]
         password = request.form["passw"]
         info = User.query.filter_by(User_name=name).all()
-        
+        print("check play load")
+        print(session)
         if info == []:
             flash('Wrong')
             return render_template('home.html')
@@ -43,15 +46,20 @@ def login():
             }
             for i in info:
                 if name == i.User_name and check_password_hash(i.user_password, password):
-                    #login_user(i)
-                    print(session) 
-                    sessions['user_role']=Role.query.get(i.role_id).role_name
-                    print(sessions)
-                    if sessions['user_role']=='admin':
+                    login_user(i)
+                    print("created login user")
+                    print(session)
+                
+                    
+                    
+                    if Role.query.get(i.role_id).role_name=='admin':
+                        print("before redirect")
+                        print(session)
+                        session.permanent=True
                         return  redirect(url_for('admin.admin'))
-                    elif sessions['user_role']=='host':
+                    elif Role.query.get(i.role_id).role_name=='host':
                         return  redirect(url_for('host.dashboard',idi=i.user_id))
-                    elif sessions['user_role']=='student':
+                    elif Role.query.get(i.role_id).role_name=='student':
                         return  redirect(url_for('student.student_dash',idi=i.user_id))
 
 
@@ -59,7 +67,13 @@ def login():
             flash("invalid cardinals")
             return render_template('home.html')
     
-
+@login_required
+@log.route('/log/out')
+def logout():
+    logout_user()
+    session.clear()
+    print(session)
+    return redirect(url_for('log.login'))
 @log.route('/acc/create', methods=['GET', 'POST'])
 def create_acc():
     if request.method == "GET":
